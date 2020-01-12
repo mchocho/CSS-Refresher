9be8104efedd8793ba2d579e14c8429ab9f0347a
sal'e chocho <malachocho01@gmail.com>
2020-01-12 16:12:32 +0200

    Initial commit

---
+++ refresher.js
@@ -0,0 +1,20 @@
+function script() {
+	function reloadCSS() {
+		const links = document.getElementsByTagName('link');
+		console.log('Refreshing CSS');
+		for (let i = 0, n = links.length; i < n; i++) {
+			if (links[i].href.toLowerCase().indexOf('cdn') === -1)
+				links[i].href += "?";
+                }
+		console.log('CSS page refreshed.');
+                return;
+        };
+	
+	window.addEventListener('keyup', function(e) {
+		if (e.keyCode === 101 || e.keyCode === 53) {    //Numpad 5
+			reloadCSS();
+		}
+		return;
+	});
+}
+document.addEventListener("DOMContentLoaded", script);
